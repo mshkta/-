b7d18f3383324957f05fc0ce73ec2b0eeb8f487d
mshkta <mshkta003@gmail.com>
2025-05-29 13:40:30 +0900

    Create card.js

---
+++ components/ui/card.js
@@ -0,0 +1,13 @@
+export function Card({ children }) {
+  return (
+    <div className="border rounded shadow-sm bg-white">{children}</div>
+  );
+}
+
+export function CardContent({ children, className = "" }) {
+  return (
+    <div className={`p-4 ${className}`}>
+      {children}
+    </div>
+  );
+}
